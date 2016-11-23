e63694e1706d45af46e9493f79c71d28facba606
NWOskar <a00208226@student.ait.ie>
2016-11-23 10:25:34 +0000

    V3- Number out of the scale Done

--- Documents/Java/Conventer/src/Conventer/Conventer.java
+++ Documents/Java/Conventer/src/Conventer/Conventer.java
@@ -13,8 +13,6 @@ public class Conventer {
 		} catch(Exception e) {
             System.out.println("Only numbers are accepeted");
         }
-			
-		
 		
 		if((temp > 100)||(temp < -273) )
 		{
@@ -30,7 +28,13 @@ public class Conventer {
 	
 	public int farenheit(int ce)
 	{
-		int temp = (int) ((ce * 9 / 5.0) + 32);
+		int temp =0;
+		try{
+			temp = (int) ((ce * 9 / 5.0) + 32);
+
+		} catch(Exception e) {
+            System.out.println("Only numbers are accepeted");
+        }
 		
 		if((temp > 212)||(temp < -460) )
 		{
